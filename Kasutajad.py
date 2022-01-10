cc6df44a23f12435eda1d83178799be9e1af00fb
opilane <opilane@HP-CZC6297BB1>
2022-01-10 09:41:14 +0200

    dsadsadasdasd

--- Kasutajad.py
+++ Kasutajad.py
@@ -1,5 +1,8 @@
 from module1 import *
 
+users=["Denis"]
+passwords=["12345"]
+
 while True:
     print("Зарегистрироваться-1, Авторизоваться-2, Выход-3")
     ch=int(input())
@@ -15,6 +18,7 @@ while True:
         ch=input("Самим записать пароль(S) или робот придумает(R)? ")
         if ch.upper()=="R":
             pas=randompass()
+            print(pas)
         elif ch.upper()=="S":
             while 1:
                 pas=input("Введите свой пароль: ")
@@ -23,8 +27,10 @@ while True:
                     users.append(log)
                     passwords.append(pas)
                     break
-    elif ch==2:
+    elif ch==2: 
         print("Вы выбрали авторизацию")
+        user=input("Введите свое имя: ")
+        pas=input("Введите свой пароль: ")
         if users.index(user)==passwords.index(pas):
             print("Добро пожаловать")
     elif ch==3:
--- module1.py
+++ module1.py
@@ -1,5 +1,3 @@
-users=["Denis"]
-passwords=["12345"]
 import random
 
 def randompass()->str:
@@ -15,3 +13,25 @@ def randompass()->str:
     random.shuffle(ls) #перемешиваются все символы
     psword = ''.join([random.choice(ls) for x in range(12)])
     return psword
+
+def control(passwords:str)->str:
+    """Проверка пароля, самое главное чтобы в личном пароле присутсвовал хотя 1 тип символов.
+    """
+    str0=".,:;!_*-+()/#¤%&"
+    alpha=digit=upper=sprcial=0
+    ls=list(str0)
+    pas=list(passwords)
+    for i in range(len(pas)):
+        if pas[i].isupper():
+            upper=1
+        if pas[i].isalpha():
+            alpha=1
+        if pas[i].isdigit():
+            digit=1
+        if pas[i] in ls:
+            special=1
+    if alpha==1 and digit==1 and upper==1 and special==1:
+        control=True
+    else:
+        control=False
+    return control
